206b346a3ccf353dba28f0ebe5a62486cadd4be4
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-03 22:53:35 +0100

    Update server.js file to configure the express-session and connect-flash libs

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const express = require('express')
 const path = require('path')
+
 require('dotenv').config()
 
 
 // settings
 const app = express()
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 3600000 } 
+}))
+app.use(bodyParser.json())
+app.use(flash())
+app.set('view engine', 'ejs')
 
 // routes
 app.use('/auth', authRoutes)
