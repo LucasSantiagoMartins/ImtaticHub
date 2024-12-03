7eae03ff4f28e445cac2bd796b7f09f92cc557dc
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-03 15:04:42 +0100

    Add student.js model file

---
+++ models/student.js
@@ -0,0 +1,49 @@
+'use strict'
+const { Model } = require('sequelize')
+
+
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model{
+        static associate(models){
+
+        }
+    }
+    Student.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+        full_name: {
+            allowNull: true,
+            type: DataTypes.STRING,
+        },
+        course: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        class: {
+            allowNull: true,
+            type: DataTypes.STRING
+        },
+        grade: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        registration_number: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        academic_year: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+    },
+    {
+        sequelize, 
+        modelName: 'Students'
+    })
+
+    return Student
+}
