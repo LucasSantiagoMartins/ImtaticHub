dac22868001312abeb19983a871b91663c2875c5
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-03 22:26:58 +0100

    Update /register-form endpoint to /register

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const authControllers = require('../controllers/authController')
 
 const router = express.Router()
 
-router.get('/register-form', authControllers.registerForm)
+router.get('/register', authControllers.register)
 router.post('/register', authControllers.register)
 router.get('/login-form', authControllers.loginForm)
 router.post('/login', authControllers.login)
