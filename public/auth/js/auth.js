322ba211eda5c34bd706ce25c92ac406b1b95005
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-05 00:08:13 +0100

    Create throwMessage, showPassword function and update form submit event to make the request to endpoint register in auth.js file

---
+++ public/auth/js/auth.js
@@ -0,0 +1,58 @@
+const form = document.querySelector('form')
+
+function throwMessage(message, status) {
+    let messageDiv = document.querySelector('#message-div')
+    
+    if ( status.ok ) {
+        messageDiv.classList.add('success')
+        messageDiv.innerHTML = `<i class="fas fa-check-circle"></i> `
+    } else {
+        messageDiv.classList.add('error')
+        messageDiv.innerHTML = `<i class="fas fa-exclamation-circle"></i> `
+    
+    }
+    messageDiv.innerHTML += message
+    $('#message-div').fadeIn(2000)
+    setTimeout(() => {
+        $('#message-div').fadeOut(1000)
+    }, 5000);
+}
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+    
+    try {
+        const response = await fetch('/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'aplication/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        const result = await response.json()
+        
+        if ( response.ok ) {
+            throwMessage( result.message, { ok: true } )
+        } else {
+            throwMessage( result.message, { ok: false } )
+        }
+
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+
+function showPassowrd(){
+    const passwordInput = document.querySelector('input[name=password]')
+
+    if ( passwordInput.type == 'password' ) {
+        passwordInput.type = 'text'
+    }else{
+        passwordInput.type = 'password'
+    }
+}
