ad1ebea65132bf536154403f95f96bfd38529a6d
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-05 20:57:20 +0100

    Add teacher.js model file

---
+++ models/teacher.js
@@ -0,0 +1,59 @@
+'use strict'
+const { Model } = require('sequelize')
+const { sequelize } = require('.')
+
+
+module.exports = (sequelize, DataTypes) => {
+    class Teacher extends Model {
+        static associate(models) {
+
+        }
+    }
+
+    Teacher.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        full_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        birth_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nationality: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+         },
+         updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+         },
+    },
+    {
+        sequelize,
+        modelName: 'Teacher',
+        tableName: 'teachers',
+        underscored: true,
+        timestamps: true
+    })
+
+    return Teacher
+}
