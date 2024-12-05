17689ceec9947ed58aa863f71f1f73daae08d5ac
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-05 22:24:37 +0100

    Add course.js model file

---
+++ models/course.js
@@ -0,0 +1,44 @@
+'use strict'
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+    class Course extends Model {
+        static associate(model) {
+
+        }
+    }
+
+    Course.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+        name: {
+            allowNull: true,
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, 
+    {
+        sequelize,
+        modelName: 'Course',
+        tableName: 'Courses',
+        underscored: true,
+        timestamps: true
+    }
+    )
+}
