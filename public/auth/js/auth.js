5a5d8bb4897d9748336507fad4d12c640fb4e10a
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-08 23:11:08 +0100

    Move the throwMessage functionto from auth.js file to general.js file

--- public/auth/js/auth.js
+++ public/auth/js/auth.js
@@ -1,24 +1,3 @@
-function throwMessage(message, status) {
-    let messageDiv = document.querySelector('#message-div')
-    
-    if ( status.ok ) {
-        messageDiv.classList.add('success')
-        messageDiv.innerHTML = `<i class="fas fa-check-circle"></i> `
-    } else {
-        messageDiv.classList.add('error')
-        messageDiv.innerHTML = `<i class="fas fa-exclamation-circle"></i> `
-    
-    }
-    messageDiv.innerHTML += message
-    $('#message-div').fadeIn(4000)
-    setTimeout(() => {
-        $('#message-div').fadeOut(500)
-        messageDiv.classList.remove('success')
-        messageDiv.classList.remove('error')
-    }, 5000);
-
-}
-
 function showPassowrd(){
     const passwordInput = document.querySelector('input[name=password]')
 
