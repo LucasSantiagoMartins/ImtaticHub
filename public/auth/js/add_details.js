5222771e4e8c8de49c4e856c0cc30aceff34dded
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-08 20:02:38 +0100

    Add form data validations in add_details.ejs view and add margin in all inputs and selects

--- public/auth/js/add_details.js
+++ public/auth/js/add_details.js
@@ -19,3 +19,37 @@ teacherCardBtn.addEventListener('click', () => {
         show('#teacher-form-div')
     }, 200)
 })
+
+
+// form data validations
+const studentFormInputs = studentForm.querySelectorAll('input')
+const studentFormSelects = studentForm.querySelectorAll('select')
+
+const teacherFormInputs = teacherForm.querySelectorAll('input')
+const teacherFormSelects = teacherForm.querySelectorAll('select')
+
+const formSelects = Array.from(studentFormSelects).concat(Array.from(teacherFormSelects))
+const formInputs = Array.from(studentFormInputs).concat(Array.from(teacherFormInputs))
+
+formInputs.forEach(input => {
+
+    input.addEventListener('change', () => {
+        if (input.value != '') {
+            input.classList.add('is-valid')
+        }else{
+            input.classList.remove('is-valid')
+            input.classList.add('is-invalid')
+        }
+    })
+})
+
+formSelects.forEach(select => {
+    select.addEventListener('change', () => {
+        if (select.value != '') {
+            select.classList.add('is-valid')
+        }else{
+            select.classList.remove('is-valid')
+            select.classList.add('is-invalid')
+        }
+    })
+})
