3143a82e02ad5b441b506558990140cba7ccb59c
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-08 23:25:51 +0100

    Add /add-details endpoint in authRoutes.js file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,6 @@ router.get('/register', authControllers.register)
 router.post('/register', authControllers.register)
 router.get('/login', authControllers.login)
 router.post('/login', authControllers.login)
+router.get('/add-details', authControllers.addDetails)
 
 module.exports = router
