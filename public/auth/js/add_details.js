5c212484e5cd363fca7d1e28db09801120284de8
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-08 13:21:07 +0100

    Add add_details.ejs view file

---
+++ public/auth/js/add_details.js
@@ -0,0 +1,21 @@
+studentCardBtn = document.querySelector('#student-card-btn')
+teacherCardBtn = document.querySelector('#teacher-card-btn')
+studentForm = document.querySelector('#student-form')
+teacherForm = document.querySelector('#teacherForm')
+
+
+studentCardBtn.addEventListener('click', () => {
+    hide('.cards')
+    setTimeout(() => {
+        show('#add-details-forms')
+        show('#student-form')
+    }, 200)
+})
+
+teacherCardBtn.addEventListener('click', () => {
+    hide('.cards')
+    setTimeout(() => {
+        show('#add-details-forms')
+        show('#teacher-form')
+    }, 200)
+})
