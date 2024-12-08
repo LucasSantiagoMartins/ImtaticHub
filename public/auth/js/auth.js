70d2e58f6a58f022bff10098f712cf746da7301b
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-08 23:08:07 +0100

    Remove request handling block from auth.js file

--- public/auth/js/auth.js
+++ public/auth/js/auth.js
@@ -1,5 +1,3 @@
-const form = document.querySelector('form')
-
 function throwMessage(message, status) {
     let messageDiv = document.querySelector('#message-div')
     
@@ -20,50 +18,6 @@ function throwMessage(message, status) {
     }, 5000);
 
 }
-
-form.addEventListener('submit', async (event) => {
-    event.preventDefault()
-
-    const overlaySpinloaderDiv = document.querySelector('.overlay')
-    overlaySpinloaderDiv.style = 'display: flex;'
-
-    const formData = new FormData(form)
-    const data = Object.fromEntries(formData.entries())
-    
-    try {
-        await fetch(form.action, {
-            method: form.method,
-            headers: {
-                'Content-Type': 'application/json', 
-            },
-            body: JSON.stringify(data)
-        }).then(async (response) => {
-            const result = await response.json()
-        
-            if ( response.ok ) {
-                throwMessage( result.message, { ok: true } )
-
-                setTimeout(() => {
-                    authRedirect(form)
-                }, 2000);
-
-            } else {
-                throwMessage( result.message, { ok: false } )
-            }
-
-
-        }).catch((err) => {
-            console.log(err)
-        }).finally(() => {
-            setTimeout(() => {
-                $('.overlay').fadeOut(500)
-            }, 500)
-        })
-    } catch(err) {
-        console.log(err)
-    }
-})
-
 
 function showPassowrd(){
     const passwordInput = document.querySelector('input[name=password]')
