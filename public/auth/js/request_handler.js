ff10dcee9ccd9d134efe79462230fd45ef9589d0
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-09 20:24:52 +0100

    Update request_handler.js file

--- public/auth/js/request_handler.js
+++ public/auth/js/request_handler.js
@@ -1,49 +1,41 @@
-const forms = document.querySelectorAll('form')
+async function requestHandler(form, redirectUrl) {
+    const overlaySpinloaderDiv = document.querySelector('.overlay')
+    overlaySpinloaderDiv.style = 'display: flex;'
 
-forms.forEach( form => {
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+        
+    try {
+
+        await fetch(form.action, {
+            method: form.method,
+            headers: {
+                'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(data)
+        }).then(async (response) => {
+        const result = await response.json()
     
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault()
+        if ( response.ok ) {
+            throwMessage( result.message, { ok: true } )
 
-        const overlaySpinloaderDiv = document.querySelector('.overlay')
-        overlaySpinloaderDiv.style = 'display: flex;'
+            setTimeout(() => {
+                redirect(redirectUrl)
+            }, 2000);
 
-        const formData = new FormData(form)
-        const data = Object.fromEntries(formData.entries())
-        
-        try {
-            await fetch(form.action, {
-                method: form.method,
-                headers: {
-                    'Content-Type': 'application/json', 
-                },
-                body: JSON.stringify(data)
-            }).then(async (response) => {
-            const result = await response.json()
-        
-            if ( response.ok ) {
-                throwMessage( result.message, { ok: true } )
-
-                setTimeout(() => {
-                    authRedirect(form)
-                }, 2000);
-
-            } else {
-                throwMessage( result.message, { ok: false } )
-            }
-
-            }).catch((err) => {
-                console.log(err)
-            }).finally(() => {
-                setTimeout(() => {
-                    $('.overlay').fadeOut(500)
-                }, 500)
-            })
-        } catch(err) {
-            console.log(err)
+        } else {
+            throwMessage( result.message, { ok: false } )
         }
-    })
-})
 
+        }).catch((err) => {
+                console.log(err)
+        }).finally(() => {
+            setTimeout(() => {
+                $('.overlay').fadeOut(500)
+            }, 500)
+        })
+    } catch(err) {
+            console.log(err)
+    }
 
-
+}
