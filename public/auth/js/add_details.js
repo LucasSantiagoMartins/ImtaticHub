47ee8eb9ab44635468db6e395014f00431a0e09a
Lucas Santiago <lucassantiagocody@gmail.com>
2024-12-11 23:02:37 +0100

    Add form_validation.js file and move all codes about form data validation from add_details.js to form_validation.js file

--- public/auth/js/add_details.js
+++ public/auth/js/add_details.js
@@ -29,39 +29,6 @@ teacherForm.addEventListener('submit', (event) => {
     requestHandler(teacherForm, redirectUrl = '/')
 })
 
-// form data validations
-const studentFormInputs = studentForm.querySelectorAll('input')
-const studentFormSelects = studentForm.querySelectorAll('select')
-
-const teacherFormInputs = teacherForm.querySelectorAll('input')
-const teacherFormSelects = teacherForm.querySelectorAll('select')
-
-const formSelects = Array.from(studentFormSelects).concat(Array.from(teacherFormSelects))
-const formInputs = Array.from(studentFormInputs).concat(Array.from(teacherFormInputs))
-
-formInputs.forEach(input => {
-
-    input.addEventListener('change', () => {
-        if (input.value != '') {
-            input.classList.add('is-valid')
-        }else{
-            input.classList.remove('is-valid')
-            input.classList.add('is-invalid')
-        }
-    })
-})
-
-formSelects.forEach(select => {
-    select.addEventListener('change', () => {
-        if (select.value != '') {
-            select.classList.add('is-valid')
-        }else{
-            select.classList.remove('is-valid')
-            select.classList.add('is-invalid')
-        }
-    })
-})
-
 const path = window.location.pathname
 const segments = path.split('/')
 const userId = segments[segments.length - 1]
